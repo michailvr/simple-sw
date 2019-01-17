4607ee0a6abf24c85d9021eb1b8b8e0af0c1ff45
hronik <mrubinskiy@gmail.com>
2019-01-18 01:08:25 +0700

    second commit

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 if ('serviceWorker' in navigator) {
+    let button = document.querySelector('#unreg');
     navigator.serviceWorker.register('./simple-sw.js')
              .then(value => {
-                 console.log('Service worker registered! ', value);
+                 console.log('Service worker register! ', value);
+                 button.disabled = false;
+                 button.addEventListener('click', e => {
+                     value.unregister()
+                         .then(unreg => {
+                             console.log('Service worker unregister! ', value);
+                         });
+                 });
                  Notification.requestPermission()
                              .then(v => {
                                  console.log('Notification: ', v);
